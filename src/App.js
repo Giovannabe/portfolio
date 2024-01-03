5c29fcd7c329396fc67ea5aa8b767cb6ecb47256
Giovannabe <giovannaaeleodoraaberaldoo@gmail.com>
2024-01-02 23:50:20 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <xu>
+      <header>
+        <div className='fundo'>
+
+        <div className='borda'>
+           <img src='/imagens/hili.jpg' />
+        </div>
+
+           <h2>Giovanna</h2>
+
+        </div>
+        <div className='sobre'>
+          <p>Welcome to my digital garden where I share what 
+              I'm learning about <br></br>shipping great products, becoming a better developer and growing a career in tech.</p>
+        </div>
+      </header>
+
+
+    </xu>
+  );
+
+}
+
+export default App;
