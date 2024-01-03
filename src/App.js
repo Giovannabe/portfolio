d976cc2a8ebf2d89a2d1c7d76389d686e8290fc4
Giovannabe <giovannaaeleodoraaberaldoo@gmail.com>
2024-01-03 01:16:30 -0300

    adcionei imagens, alterei texto, css

--- src/App.js
+++ src/App.js
@@ -7,19 +7,34 @@ function App() {
         <div className='fundo'>
 
         <div className='borda'>
-           <img src='/imagens/hili.jpg' />
+           <img src='/imagens/foto.jpg' />
         </div>
 
-           <h2>Giovanna</h2>
+           <h2>Giovanna Eleodora Beraldo</h2>
 
         </div>
         <div className='sobre'>
           <p>Welcome to my digital garden where I share what 
               I'm learning about <br></br>shipping great products, becoming a better developer and growing a career in tech.</p>
         </div>
+
+        <div className='icones'>
+         <a href='https://github.com/Giovannabe' target='_blank'><img src='/imagens/github.png' /></a>
+         <a href='https://github.com/Giovannabe' target='_blank'><img src='/imagens/linkedin.png' /></a>
+         <a href='https://github.com/Giovannabe' target='_blank'><img src='/imagens/e-mail.png' /></a>
+
+        </div>
       </header>
 
+<div className='todos'>
+      <div className='primeirocont'>
+         <h2>My first project</h2>
+      </div>
+</div>
 
+<footer>
+  <p>Made by GiovannaBe</p>
+</footer>
     </xu>
   );
 
